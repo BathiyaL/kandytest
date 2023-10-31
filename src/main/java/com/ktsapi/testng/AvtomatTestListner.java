8c12b64a847d191c57201a56364daffb3a66e653
Bathiya Ladduwahetty <dbladduwahetty@gmail.com>
2023-10-31 23:42:03 +1300

    KTSA-#6 : add initial report template

--- src/main/java/com/ktsapi/testng/AvtomatTestListner.java
+++ src/main/java/com/ktsapi/testng/AvtomatTestListner.java
@@ -1,17 +1,37 @@
 package com.ktsapi.testng;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Method;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
+import java.util.Calendar;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 import org.testng.IClass;
 import org.testng.IConfigurationListener2;
+import org.testng.IReporter;
+import org.testng.ISuite;
+import org.testng.ISuiteResult;
 import org.testng.ITestContext;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
 import org.testng.SkipException;
 import org.testng.annotations.BeforeTest;
+import org.testng.xml.XmlSuite;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
@@ -26,7 +46,7 @@ import com.ktsapi.enums.TestResultStatus;
 import com.ktsapi.kclient.KandyClientApiCaller;
 import com.ktsapi.utils.AvtomatUtils;
 
-public class AvtomatTestListner implements ITestListener, IConfigurationListener2 {
+public class AvtomatTestListner implements ITestListener, IConfigurationListener2, IReporter {
 
 	private Runner runner = null;
 	LocalDateTime testExecutionStartTime;
@@ -304,6 +324,144 @@ public class AvtomatTestListner implements ITestListener, IConfigurationListener
 
 	public long getTestCount() {
 		return this.testCount;
+	}
+
+	// ########################################################################################################3
+
+
+	//private static final Logger LOGGER = LoggerFactory.getLogger(CustomisedReports.class);
+	private static final String REPORT_NAME = "testreport.html";
+	private static final String REPORT_FOLDER= "kandyreports";
+
+	//private static final String ROW_TEMPLATE = "<tr class=\"%s\"><td>%s</td><td>%s</td><td>%s</td><td>%s</td><td>%s</td></tr>";
+	private static final String ROW_TEMPLATE = "<tr><td>%s</td><td>%s</td><td>%s</td><td class=\"%s\">%s</td><td style=\"text-align: center; vertical-align: middle;\">%s</td></tr>";
+
+	public void generateReport(List<XmlSuite> xmlSuites, List<ISuite> suites, String outputDirectory) {
+		String reportTemplate = initReportTemplate();
+		
+		String suiteName = suites.get(0).getName();	
+    	int testsCount = 0;
+        int passedTests = 0;
+        int skippedTests = 0;
+        int failedTests = 0;
+		
+		for(String key :  suites.get(0).getResults().keySet()) {
+			//System.out.println("=================================== :: " + key);
+	    	testsCount += suites.get(0).getResults().get(key).getTestContext().getPassedTests().size();
+	        passedTests += suites.get(0).getResults().get(key).getTestContext().getPassedTests().size();
+	        skippedTests += suites.get(0).getResults().get(key).getTestContext().getSkippedTests().size();
+	        failedTests += suites.get(0).getResults().get(key).getTestContext().getFailedTests().size();			
+		}
+		testsCount = passedTests + skippedTests + failedTests;
+		System.out.println("===================================passedTests :: " + passedTests);
+		System.out.println("===================================skippedTests :: " + skippedTests);
+		System.out.println("===================================failedTests :: " + failedTests);
+		
+		System.out.println("===================================suiteName :: " + suiteName);
+		
+		reportTemplate = reportTemplate.replace("$reportTitle", getReportTitle(suiteName));
+		reportTemplate = reportTemplate.replace("$passedTestCount", Integer.toString(passedTests));
+		reportTemplate = reportTemplate.replace("$failedTestCount", Integer.toString(failedTests));
+		reportTemplate = reportTemplate.replace("$skippedTestCount", Integer.toString(skippedTests));
+		
+		
+		final String body = suites.stream().flatMap(suiteToResults()).collect(Collectors.joining());
+		//saveReportTemplate(outputDirectory, reportTemplate.replaceFirst("</tbody>", String.format("%s</tbody>", body)));
+		saveReportTemplate(outputDirectory, reportTemplate.replace("$testResults",body));
+	}
+	
+    protected String getReportTitle(String title) {
+		return title + " [" + getCurrentDateTime() + "]" ;
+	}
+
+	private Function<ISuite, Stream<? extends String>> suiteToResults() {
+		return suite -> suite.getResults().entrySet().stream().flatMap(resultsToRows(suite));
+	}
+
+	private Function<Map.Entry<String, ISuiteResult>, Stream<? extends String>> resultsToRows(ISuite suite) {
+		return e -> {
+			ITestContext testContext = e.getValue().getTestContext();
+
+			Set<ITestResult> failedTests = testContext.getFailedTests().getAllResults();
+			Set<ITestResult> passedTests = testContext.getPassedTests().getAllResults();
+			Set<ITestResult> skippedTests = testContext.getSkippedTests().getAllResults();
+			
+			// testContext.getFailedTests().getAllMethods().getAllTestMethods()[0].getDescription();			
+
+			String suiteName = suite.getName();
+
+			return Stream.of(failedTests, passedTests, skippedTests)
+					.flatMap(results -> generateReportRows(e.getKey(), suiteName, results).stream());
+		};
+	}
+
+	private List<String> generateReportRows(String testName, String suiteName, Set<ITestResult> allTestResults) {
+		return allTestResults.stream().map(testResultToResultRow(testName, suiteName)).toList();
+	}
+
+	private Function<ITestResult, String> testResultToResultRow(String testName, String suiteName) {
+		// testResult.getName() // test method name
+		// testResult.getMethod().getDescription() // test description
+		// testResult.getName() // test method name
+		return testResult -> {
+			
+			String fullyQualifiedName = testResult.getTestClass().getName();
+			String testClassName = fullyQualifiedName.substring(fullyQualifiedName.lastIndexOf(".")+1);
+			String testGroup = testName;
+			String testDescription = testResult.getMethod().getDescription();
+			
+			switch (testResult.getStatus()) {
+			case ITestResult.FAILURE:
+				return String.format(ROW_TEMPLATE, testGroup, testClassName, testDescription,"bg-danger", "FAILED", "NA");
+
+			case ITestResult.SUCCESS:
+				return String.format(ROW_TEMPLATE, testGroup, testClassName, testDescription,"bg-success", "PASSED",
+						String.valueOf(testResult.getEndMillis() - testResult.getStartMillis()));
+
+			case ITestResult.SKIP:
+				return String.format(ROW_TEMPLATE,testGroup, testClassName,"bg-warning", testDescription, "SKIPPED",
+						"NA");
+
+			default:
+				return "";
+			}
+		};
+	}
+
+	private String initReportTemplate() {
+		String template = null;
+		byte[] reportTemplate;
+		Path resourceDirectoryPath = Paths.get("src","test","resources","reportTemplateV4.html");
+		try {
+			//reportTemplate = Files.readAllBytes(Paths.get("src/test/resources/reportTemplateV4.html"));
+			reportTemplate = Files.readAllBytes(Paths.get(resourceDirectoryPath.toUri()));
+			template = new String(reportTemplate, "UTF-8");
+		} catch (IOException e) {
+			//LOGGER.error("Problem initializing template", e);
+		}
+		return template;
+	}
+
+	private void saveReportTemplate(String outputDirectory, String reportTemplate) {
+		
+		// create custom report path to hold custom reports only
+		File outputDirectoryFilePath = new File(outputDirectory,REPORT_FOLDER);
+		outputDirectoryFilePath.mkdirs();
+		try {
+			PrintWriter reportWriter = new PrintWriter(
+					new BufferedWriter(new FileWriter(new File(outputDirectoryFilePath.getPath(), REPORT_NAME))));
+			reportWriter.println(reportTemplate);
+			reportWriter.flush();
+			reportWriter.close();
+		} catch (IOException e) {
+			//LOGGER.error("Problem saving template", e);
+		}
+	}
+	
+	public static String getCurrentDateTime() {
+		Calendar currentDate = Calendar.getInstance();
+		SimpleDateFormat formatter = new SimpleDateFormat("dd-MMM-yyyy:HH.mm.ss");
+		return formatter.format(currentDate.getTime());
 	}
 
 }
